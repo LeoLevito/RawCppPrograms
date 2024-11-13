9e4f39a1376431a3b4780622b59c794903a6d803
LeoLevito <theleolevito@gmail.com>
2024-11-13 15:44:17 +0100

    [OGLEngine] added necessary imgui files. [main.cpp] changed Graphics variable to be a pointer.

--- OGLEngine/OGLEngine/main.cpp
+++ OGLEngine/OGLEngine/main.cpp
@@ -4,22 +4,22 @@
 
 int main()
 {
-	Graphics graphics;
-	graphics.Initialize(640, 480);
+	Graphics* graphics = new Graphics;
+	graphics->Initialize(640, 480);
 
 	Engine* engine = new Engine;
-	engine->Initialize(graphics.window, graphics.myCamera);
+	engine->Initialize(graphics->window, graphics->myCamera);
 
 	float lastTime = 0;
 	float currentTime = 0;
 	float deltaTime = 0;
 
-	while (!graphics.ShouldClose())
+	while (!graphics->ShouldClose())
 	{
 		currentTime = glfwGetTime(); //May or may not be super accurate at the moment.
 		deltaTime = currentTime - lastTime;
 		lastTime = currentTime;
-		graphics.Render();
+		graphics->Render();
 		engine->Update(deltaTime);
 	}
 	return 0;
